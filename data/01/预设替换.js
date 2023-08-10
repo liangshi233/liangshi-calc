a5a1670c5f94636538559ace76247b720b9b98db
liangshi <1142607614@qq.com>
2023-08-10 23:37:55 +0800

    预设面板使用体验优化 By 小花花花儿

--- data/01/预设替换.js
+++ data/01/预设替换.js
@@ -1,7 +1,6 @@
 //如果自己有或者自己写了替换插件可以不用复制本JS
 let replace_list = {
 	'极限': '极限',
-	'极限面板': '面板100000000',
 	'极限伤害': '伤害100000000',
 	'核爆面板': '面板100000001',
 	'核爆伤害': '伤害100000001',
@@ -40,7 +39,7 @@ export class xiaofei_input_replace extends plugin {
 				if (!reg.test(e.msg)) continue;
 				if (/^(#)?极限/.test(e.msg)) {
                     e.msg = e.msg.replace(/^(#)?极限/, '#');
-                    e.msg = `${e.msg}极限面板`;
+                    e.msg = `${e.msg}面板100000000`;
 				}
 				e.msg = e.msg.replace(reg, replace_list[key]);
 			} catch (err) { }
