19ea7e7f9762d6aacf9c9498342e324ca1db4d65
liangshi <1142607614@qq.com>
2023-09-30 02:04:37 +0800

    部分角色组队伤害计算

--- damage/meta/芙宁娜/calc_liangshi.js
+++ damage/meta/芙宁娜/calc_liangshi.js
@@ -1,57 +1,60 @@
+import { Format } from '../../../../../plugins/liangshi-calc/components/index.js'
+
 export const details = [{
   check: ({ cons }) => cons >= 2,
-  title: '普通攻击一段',
-  dmg: ({ talent }, dmg) => dmg(talent.a['普通攻击伤害'], 'a')
+  title: '2命普攻伤害提升值-荒',
+  dmg: ({  calc, attr }) => {
+    return {
+      avg: Format.number ( ( calc(attr.hp) * 0.3 ) ) ,
+      type: 'text'
+    }
+  }
 }, {
   title: '众水的歌者治疗量',
   dmg: ({ talent, attr, calc }, { heal }) =>
     heal(talent.e['众水的歌者治疗量2'][0] * calc(attr.hp) / 100 + talent.e['众水的歌者治疗量2'][1] * 1)
 }, {
   title: '海薇玛夫人伤害',
-  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['海薇玛夫人伤害'] / 100, 'e')
-},{
-  title: '海薇玛夫人蒸发',
-  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['海薇玛夫人伤害'] / 100, 'e', '蒸发')
-}, {
-  title: '谢贝蕾妲小姐伤害',
-  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['谢贝蕾妲小姐伤害'] / 100, 'e')
-},{
-  title: '谢贝蕾妲小姐蒸发',
-  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['谢贝蕾妲小姐伤害'] / 100, 'e', '蒸发')
+  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['海薇玛夫人伤害'] / 100 * 1.4 , 'e')
 }, {
   title: '乌瑟勋爵伤害',
-  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['乌瑟勋爵伤害'] / 100, 'e')
+  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['乌瑟勋爵伤害'] / 100 * 1.4 , 'e')
+}, {
+  title: '谢贝蕾妲小姐伤害',
+  dmgKey: 'e',
+  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['谢贝蕾妲小姐伤害'] / 100 * 1.4 , 'e')
 },{
-  title: '乌瑟勋爵蒸发',
-  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['乌瑟勋爵伤害'] / 100, 'e', '蒸发')
+  title: '谢贝蕾妲小姐蒸发',
+  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['谢贝蕾妲小姐伤害'] / 100 * 1.4 , 'e', '蒸发')
+}, {
+  title: '乌瑟勋爵伤害',
+  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['乌瑟勋爵伤害'] / 100 * 1.4 , 'e')
 }, {
   title: '舞台展开伤害',
-  dmg: ({ talent, attr, calc, cons }, { basic }) => {
-    return basic(calc(attr.hp) * (talent.q['技能伤害'] / 100), 'q')
-  }
+  dmg: ({ talent, attr, calc, cons }, { basic }) => basic(calc(attr.hp) * (talent.q['技能伤害'] / 100), 'q')
 }, {
   title: 'Q展开蒸发伤害',
-  dmg: ({ talent, attr, calc, cons }, { basic }) => {
-    return basic(calc(attr.hp) * (talent.q['技能伤害'] / 100), 'q', '蒸发')
-  }
+  dmg: ({ talent, attr, calc, cons }, { basic }) => basic(calc(attr.hp) * (talent.q['技能伤害'] / 100), 'q', '蒸发')
 }, {
-  title: '天赋生命恢复',
-  dmg: ({ calc, attr, talent }, { heal }) => heal(calc(attr.hp) * 0.02)
+  title: '心夜万芙 谢贝蕾妲',
+  params: { team: true },
+  dmg: ({ talent, attr, calc }, { basic }) => basic(calc(attr.hp) * talent.e['谢贝蕾妲小姐伤害'] / 100 * 1.4 , 'e')
+}, {
+  title: '心夜万芙 舞台展开',
+  params: { team: true },
+  dmg: ({ talent, attr, calc, cons }, { basic }) => basic(calc(attr.hp) * (talent.q['技能伤害'] / 100), 'q')
 }]
 
 
 export const mainAttr = 'hp,cpct,cdmg'
+export const defDmgKey = 'e'
 
-export const buffs = [{
-  title: '芙宁娜天赋：消耗队友生命值提升E[eMulti]%伤',
-  data: {
-    eMulti: 40
-  }
-}, {
+
+export const buffs = [{title: '芙宁娜天赋：消耗队友生命值提升E伤害至原来的140%'}, {
   title: '芙宁娜天赋：450层提升[dmg]%伤害与[heal]%治疗加成',
   data: {
-    dmg: ({ talent }) => talent.q['伤害提高'] * 450 ,
-    heal: ({ talent }) => talent.q['治疗加成提高'] * 450
+    dmg: ({ talent }) => talent.q['气氛值转化提升伤害比例'] * 450 ,
+    heal: ({ talent }) => talent.q['气氛值转化治疗加成比例'] * 450
   }
 }, {
   title: '芙宁娜天赋2：基于生命值提升召唤物提升[eDmg]%伤害',
@@ -60,21 +63,75 @@ export const buffs = [{
   }
 }, {
   title: '芙宁娜1命：层数上限提升150层',
+  cons: 1,
   data: {
-    dmg: ({ talent }) => talent.q['伤害提高'] * 150 ,
-    heal: ({ talent }) => talent.q['治疗加成提高'] * 150
+    dmg: ({ talent }) => talent.q['气氛值转化提升伤害比例'] * 150 ,
+    heal: ({ talent }) => talent.q['气氛值转化治疗加成比例'] * 150
   }
 }, {
-  title: '芙宁娜2命：基于生命值的15%伤害值提升[aPct]点',
+  title: '芙宁娜2命：基于生命值的30%提升普通攻击伤害值[aPct]点',
+  cons: 2,
   data: {
-    aPct: ({ calc, attr }) => calc(attr.hp) * 0.15,
-    a2Pct: ({ calc, attr }) => calc(attr.hp) * 0.15
+    aPct: ({ calc, attr }) => calc(attr.hp) * 0.3,
+    a2Pct: ({ calc, attr }) => calc(attr.hp) * 0.3
   }
 }, {
   title: '芙宁娜6命：每层提升芙宁娜0.4%生命值上限，共提升[hpPct]%',
+  cons: 6,
   data: {
     hpPct: ({ calc, attr }) =>  Math.min( 140 , ( calc(attr.hp) * 0.004 * 600 ) )
   }
+}, {
+  check: ({ cons, params }) => cons <= 3 && params.team === true,
+  title: '0命夜兰：获得[dmg]%增伤',
+  data: {
+    dmg: 50
+  }
+}, {
+  check: ({ cons, params }) => (cons >= 4 && params.team === true),
+  title: '4命夜兰：获得[dmg]%增伤,增加40%生命值',
+  data: {
+    dmg: 50,
+    hpPct: 40
+  }
+}, {check: ({ cons,params }) => cons <= 1 && params.team === true,
+    title: '精1苍古0命万叶：获得[dmg]%增伤(苍古普攻16增伤)，增加[atkPct]%攻击,减抗[kx]%',
+    data: {
+      aDmg:16,
+      a2Dmg:16,
+      a3Dmg:16,
+      dmg: 40,
+      atkPct:20,
+      kx:40,
+   }
+  }, {check: ({ cons,params }) => ((cons < 6 && cons >1) && params.team === true),
+    title: '精1苍古2命万叶：获得[dmg]%增伤(苍古普攻16增伤)，增加[atkPct]%攻击,减抗[kx]%,精通[mastery]',
+    data: {
+      aDmg:16,
+      a2Dmg:16,
+      a3Dmg:16,
+      dmg: 48,
+      atkPct:20,
+      kx:40,
+      mastery:200
+   }
+  }, {check: ({ cons,params }) =>  (cons >= 6 && params.team === true),
+    title: '精5苍古6命万叶：获得[dmg]%增伤(苍古普攻32增伤)，增加[atkPct]%攻击,减抗[kx]%,精通[mastery]',
+    data: {
+      aDmg:32,
+      a2Dmg:32,
+      a3Dmg:32,
+      dmg: 48,
+      atkPct:40,
+      kx:40,
+      mastery:200
+   }
+  }, {
+  check: ({ params }) => params.team === true,
+  title: '元素共鸣 愈疗之水：生命值上限提升[hpPct]%',
+  data: {
+    hpPct: 25
+  }
 }, 'vaporize',
-{title: '9.27最后修改：如有问题可联系1142607614反馈'}
+{title: '9.29最后修改：如有问题可联系1142607614反馈'}
 ]
