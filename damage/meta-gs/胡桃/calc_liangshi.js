15a3d55346ff90d3b03f0bc9caf7361e7a3e8e3d
liangshi <1142607614@qq.com>
2023-11-06 20:49:27 +0800

    那我肯定受不了的

--- damage/meta-gs/胡桃/calc_liangshi.js
+++ damage/meta-gs/胡桃/calc_liangshi.js
@@ -1,117 +1,111 @@
-export const details = [{
+export const details = [
+{
   title: '半血开E普攻一段',
-  params: {team: false , hb: false},
-  dmg: ({ talent, attr }, dmg) => dmg(talent.a['一段伤害'], 'a')
-},{
+  dmg: ({ talent, attr }, dmg ) => dmg(talent.a['一段伤害'], 'a')
+},
+{
   title: '半血开E重击',
-  params: {team: false , hb: false},
-  dmg: ({ talent, attr }, dmg) => dmg(talent.a['重击伤害'], 'a2')
-}, {
+  dmg: ({ talent, attr }, dmg ) => dmg(talent.a['重击伤害'], 'a2')
+},
+{
   title: '半血开E普攻一段蒸发',
-  params: {team: false , hb: false},
-  dmg: ({ talent, attr }, dmg) => dmg(talent.a['一段伤害'], 'a', 'vaporize')
-}, {
+  dmg: ({ talent, attr }, dmg ) => dmg(talent.a['一段伤害'], 'a', 'vaporize')
+},
+{
   title: '半血开E重击蒸发',
-  params: {team: false , hb: false},
-  dmg: ({ talent, attr }, dmg) => dmg(talent.a['重击伤害'], 'a2', 'vaporize')
-}, {
+  dmg: ({ talent, attr }, dmg ) => dmg(talent.a['重击伤害'], 'a2', 'vaporize')
+},
+{
   title: '血梅香伤害',
-  params: {team: false , hb: false},
-  dmg: ({ talent, attr }, dmg) => dmg(talent.e['血梅香伤害'], 'e')
-}, {
+  dmg: ({ talent, attr }, dmg ) => dmg(talent.e['血梅香伤害'], 'e')
+},
+{
   title: '半血开E后Q',
-  params: {team: false , hb: false},
-  dmg: ({ talent, attr }, dmg) => dmg(talent.q['低血量时技能伤害'], 'q')
-}, {
+  dmg: ({ talent, attr }, dmg ) => dmg(talent.q['低血量时技能伤害'], 'q')
+},
+{
   title: '胡行夜钟 重击蒸发',
-  params: {team: true , hb: false},
-  dmg: ({ talent, attr }, dmg) => dmg(talent.a['重击伤害'], 'a2', 'vaporize')
-}, {
+  params: { teamA: true },
+  dmg: ({ talent, attr }, dmg ) => dmg(talent.a['重击伤害'], 'a2', 'vaporize')
+},
+{
   title: '胡行夜钟 Q蒸发',
-  params: {team: true , hb: false},
-  dmg: ({ talent, attr }, dmg) => dmg(talent.q['低血量时技能伤害'], 'q', 'vaporize')
-}, {
-  title: '深渊7-2 Q融化核爆',
-  params: {team: false , hb: true},
-  dmg: ({ talent, attr }, dmg) => dmg(talent.q['低血量时技能伤害'], 'q', 'melt')
-}, {
-  title: '胡行夜钟 21轴 对单',
-  params: { team: true , hb: false },
-  dmg: ({ talent }, dmg) => {
-    let pg = dmg(talent.a['一段伤害'], 'a', 'vaporize')
-    let zj = dmg(talent.a['重击伤害'], 'a2', 'vaporize')
-    let xmx = dmg(talent.e['血梅香伤害'], 'e', 'vaporize')
-    let q = dmg(talent.q['低血量时技能伤害'], 'q', 'vaporize')
-    return {
-      dmg: 10 * pg.dmg +  9 * zj.dmg  +  5 * xmx.dmg  + 1 * q.dmg,
-      avg: 10 * pg.avg +  9 * zj.avg  +  5 * xmx.avg  + 1 * q.avg
-    }
-  }
+  params: { teamA: true },
+  dmg: ({ talent, attr }, dmg ) => dmg(talent.q['低血量时技能伤害'], 'q', 'vaporize')
 }]
 
 export const defDmgIdx = 3
 export const mainAttr = 'hp,atk,cpct,cdmg,mastery'
 
-export const defParams = {
-  team: true,
-  hb: true
-}
-
-
-export const buffs = [{
-  title: '蝶引来生：开E获得[atkPlus]点攻击力加成',
+export const buffs = [
+{
+  title: '胡桃技能：[蝶引来生] 消耗一部分生命值,击退周围敌人,基于进入该状态时胡桃的生命值上限,提高胡桃[atkPlus]点攻击力',
   data: {
     atkPlus: ({ talent, attr, calc }) => {
-      return Math.min(talent.e['攻击力提高'] * calc(attr.hp) / 100, attr.atk.base * 4)
+      return Math.min( talent.e['攻击力提高'] * calc(attr.hp) / 100, attr.atk.base * 4 )
     }
   }
-}, {
-  title: '胡桃天赋2：半血获得33%火伤加成',
+},
+{
+  title: '胡桃天赋：[血之灶火] 胡桃的生命值低于或等于50%时,获得[dmg]%火元素伤害加成',
   data: {
     dmg: 33
   }
-}, {
-    title: '胡桃2命：血梅香造成的伤害提高[ePlus]',
-    data: {
-      ePlus: ({ attr, calc }) => calc(attr.hp) * 0.1
-    }
-  }, {
-  check: ({ cons, params }) => cons <= 3 && params.team === true,
-  title: '0命夜兰：获得[dmg]%增伤',
+},
+{
+  title: '胡桃1命：[赤团开时斜飞去] 处于蝶引来生施加的彼岸蝶舞状态下时,胡桃的重击体力消耗减少[_a2Stamina]% ',
+  cons: 1,
   data: {
-    dmg: 35
+    _a2Stamina: 100
   }
-}, {
-  check: ({ cons, params }) => (cons >= 4 && params.team === true),
-  title: '4命夜兰：获得[dmg]%增伤,增加40%生命值',
+},
+{
+  title: '胡桃2命：[最不安神晴又复雨] 血梅香造成的伤害提高[ePlus]点,安神秘法会为命中的敌人施加血梅香效果',
+  cons: 2,
+  sort: 9,
   data: {
-    dmg: 50,
-	hpPct: 25
+    ePlus: ({ attr, calc }) => calc(attr.hp) * 0.1
   }
-}, {
-  check: ({ params }) => params.team === true,
-  title: '钟离：降低敌人20%抗性',
+},
+{
+  title: '胡桃6命：[幽蝶能留一缕芳] 胡桃的生命值降至25%以下,或承受足以使她倒下的伤害时胡桃的所有元素抗性和物理抗性提高[_res]%,暴击率提高[_cpct]%,并提高[_interruption]%抗打断能力',
+  cons: 6,
+  data: {
+    _interruption: 100 ,
+    _cpct: 100 ,
+  	_res: 200
+  }
+},
+{
+  title: '夜兰天赋：[猜先有方] 「玄掷玲珑」存在期间，能使队伍中自己的当前场上角色造成的伤害提高至多[dmg]%',
+  check: ({ params }) => params.teamA === true,
+  data: {
+    dmg: 50
+  }
+},
+{
+  title: '夜兰4命：[诓惑者，接树移花] 依照「络命丝」标记敌人的数量,至多获得[hpPct]%生命值上限',
+  check: ({ params }) => params.teamA === true,
+  sort: 1,
+  cons: 4,
+  data: {
+    hpPct: 40
+  }
+},
+{
+  title: '钟离技能：[玉璋护盾] 处于玉璋护盾庇护下的角色使附近小范围敌人的所有元素抗性与物理抗性降低[kx]%',
+  check: ({ params }) => params.teamA === true,
   data: {
     kx: 20
   }
-}, {
-  check: ({ params }) => params.team === true,
-  title: '元素共鸣 愈疗之水：生命值上限提升[hpPct]%',
+},
+{
+  title: '元素共鸣：[愈疗之水] 生命值上限提升[hpPct]%',
+  check: ({ params }) => params.teamA === true,
   data: {
     hpPct: 25
   }
-}, {
-  check: ({ params }) => (params.team === false && params.hb === true),
-  title: '深渊核爆：各种buff',
-  data: {
-    cdmg: 120,
-    dmg: 100,
-    mastery: 370,
-    kx: 40,
-    atkPct: 93,
-    atkPlus: 1202.35
-  }
-}
-, 'vaporize',
-{title: '2.7最后修改：如有问题可联系1142607614反馈'}
+},
+ 'vaporize',
+{title: '2.7最后修改：[11.6重置]'}
 ]
