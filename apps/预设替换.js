37ebf2a092add3e85a851a088c1ac5985260d593
Rrrrrrray <7407060+Rrrrrrray@user.noreply.gitee.com>
2023-12-19 04:47:43 +0800

    🦄 refactor: 优化预设面板部分代码

--- apps/预设替换.js
+++ apps/预设替换.js
@@ -1,81 +1,88 @@
-let replace_list = {
-	//预设面板
-	'极限': '极限',
-	'核爆': '核爆',
-	'辅助': '辅助',
-	'平民': '平民',
-	'毕业': '毕业',
-	'试用': '试用',
-};
-
-export class xiaofei_input_replace extends plugin {
-	constructor() {
-		super({
-			name: '输入替换',
-			dsc: '',
-			event: 'message',
-			priority: -10000
-		})
-	}
-	async accept(e) {
-		for (let key in replace_list) {
-			try {
-				let reg = RegExp(key);
-				if (!reg.test(e.msg)) continue;
-				let msg = e.msg.split('换')
-				console.log(msg[0],msg[1],msg[2])
-				if (msg.length > 1) {
-                 let msgone = await set(e,msg[0], true)
-				 let msgtwo = await set(e,msg[1], false)
-				 e.msg = msgone + '换' + msgtwo
-				} else{
-				e.msg = await set(e,e.msg, true)
-				}
-			} catch (err) { }
-		}
-		return false;
-	}
+/**
+ * 预设面板替换关键词
+ * 顺序不可乱，对应10000000-100000005
+ */
+const replace_list = {
+  极限: '极限',
+  核爆: '核爆',
+  辅助: '辅助',
+  平民: '平民',
+  毕业: '毕业',
+  试用: '试用'
 }
 
-async function set(e,msg, s) {
-    msg = msg.replace(/#|极限|核爆|辅助|平民|毕业|试用|详情|详细/g, '')
-    let mb = `${msg}${e.msg.includes('伤害') ? '': e.msg.includes('圣遗物') ? '' : 
-	e.msg.includes('武器') ? '' : e.msg.includes('面板') ? '' : '面板'}`
-    if (/极限/.test(e.msg)) {    
-	        if (s)
-	        msg =  `#${mb}100000000`
-	        else
-	        msg = `100000000${msg}`
-	    }
-    if (/核爆/.test(e.msg)) {    
-		    if (s)
-		    msg =  `#${mb}100000001`
-	        else
-		    msg = `100000001${msg}`
-	    }
-    if (/辅助/.test(e.msg)) {    
-		    if (s)
-		    msg =  `#${mb}100000002`
-	        else
-		    msg = `100000002${msg}`
-	    }
-    if (/平民/.test(e.msg)) {    
-		    if (s)
-		    msg =  `#${mb}100000003`
-	        else
-		    msg = `100000003${msg}`
-	    }
-    if (/毕业/.test(e.msg)) {    
-		    if (s)
-		    msg =  `#${mb}100000004`
-	        else
-		    msg = `100000004${msg}`
-	    }
-    if (/试用/.test(e.msg)) {
-		    if (s)
-		    msg =  `#${mb}100000005`
-	        else
-		    msg = `100000005${msg}`
-	    }
-    return msg
+export class ysmb_input_replace extends plugin {
+  constructor () {
+    super({
+      name: '预设面板',
+      dsc: '预设面板输入替换',
+      event: 'message',
+      priority: -10000,
+      rule: [
+        {
+          /** 命令正则匹配 */
+          reg: '^#?预设面板(帮助|说明)?$', // 匹配消息正则，命令正则
+          /** 执行方法 */
+          fnc: 'ysmb'
+        }
+      ]
+    })
+  }
+
+  async accept (e) {
+    for (let key in replace_list) {
+      try {
+        let reg = RegExp(key)
+        if (!reg.test(e.msg)) continue
+        let msg = e.msg.split('换')
+        if (msg.length > 1) {
+          let msgone = await set(e, msg[0], true)
+          let msgtwo = await set(e, msg[1], false)
+          e.msg = msgone + '换' + msgtwo
+        } else {
+          e.msg = await set(e, e.msg, true)
+        }
+      } catch (err) { }
     }
+    return false
+  }
+
+  // 预设面板说明
+  async ysmb () {
+    const help = [
+      '【预设面板使用帮助】',
+      '#**极限面板',
+      '查看该角色理论最高期望伤害面板',
+      '#**核爆面板',
+      '查看该角色理论最高伤害面板',
+      '#**辅助面板',
+      '查看该角色理论最高辅助能力面板',
+      '#**平民面板',
+      '查看该角色约20个有效词条的基本参考面板',
+      '#**毕业面板(咕咕中，尽快完成）',
+      '查看该角色约28个有效词条的基本毕业面板',
+      '#**试用面板(咕咕中，尽快完成）',
+      '查看该角色在试用秘境中的面板',
+      '不输入角色名可查看该指令已支持的角色',
+      '(星铁暂只支持极限面板请等待后续更新)'
+    ]
+    await this.e.reply(help.join('\n'))
+    return true
+  }
+}
+
+async function set (e, msg, s) {
+  msg = msg.replace(/#|极限|核爆|辅助|平民|毕业|试用|详情|详细/g, '')
+  const keywords = Object.keys(replace_list)
+  if (keywords.includes(e.msg)) return e.msg
+
+  const mb = `${msg}${/伤害|圣遗物|武器|面板/.test(e.msg) ? '' : '面板'}`
+  keywords.forEach((v, k) => {
+    let reg = new RegExp(v)
+    if (reg.test(e.msg)) {
+      if (s) msg = `#${mb}10000000${k}`
+      else msg = `10000000${k}${msg}`
+    }
+  })
+  return msg
+}
