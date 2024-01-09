ea4b8e890e505e04900d5cb72ab06a139013716e
liangshi <1142607614@qq.com>
2024-01-10 00:00:35 +0800

    夏沃蕾评分

--- damage/meta-gs/班尼特/artis_liangshi.js
+++ damage/meta-gs/班尼特/artis_liangshi.js
@@ -3,7 +3,7 @@ export default function ({ attr, artis, rule, def }) {
     return rule('班尼特-治疗', { hp: 100 , atk: 25 , cpct: 0 , cdmg: 0 , mastery: 0 , dmg: 0 , recharge: 85 , heal: 100 })
   }
   if (attr.cpct * 2 + attr.cdmg >= 180 && artis.is('dmg', 4)) {
-    return rule('班尼特-输出', { hp: 50, atk: 75, cpct: 100, cdmg: 100, mastery: 75, dmg: 100, recharge: 30, heal: 50 })
+    return rule('班尼特-输出', { hp: 35, atk: 75, cpct: 100, cdmg: 100, mastery: 75, dmg: 100, recharge: 45, heal: 35 })
   }
   if ( attr.mastery >= 540 ) {
     return rule('班尼特-烈绽', { hp: 45 , cpct: 85, cdmg: 85, dmg: 35, mastery: 100 , recharge: 55 })
