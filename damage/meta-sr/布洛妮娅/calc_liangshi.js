2acb41a9f630f081c78ac2c5f6a895bfcf0588ef
liangshi <1142607614@qq.com>
2024-02-23 08:58:23 +0000

    update damage/meta-sr/布洛妮娅/calc_liangshi.js.
    Signed-off-by: liangshi <1142607614@qq.com>

--- damage/meta-sr/布洛妮娅/calc_liangshi.js
+++ damage/meta-sr/布洛妮娅/calc_liangshi.js
@@ -21,20 +21,29 @@ export const details = [{
   }
 }, {
   title: '终结技Buff爆伤提高',
+  dmgKey: 'dmg',
   dmg: ({ attr, calc, talent }) => {
     return {
-      avg: Format.percent(calc(attr.cdmg) * talent.q['百分比暴伤'] / 100 + talent.q['固定暴伤']),
-      type: 'text'
+      avg: ( calc(attr.cdmg) * talent.q['百分比暴伤'] + talent.q['固定暴伤'] * 100 ),
+      unit: '%'
     }
   }
 }]
 
+export const defDmgKey = 'dmg'
 export const mainAttr = 'atk,cpct,cdmg'
 
-export const buffs = [{
+export const buffs = [
+{
+  title: '行迹-号令：普通攻击暴击率提高至[aCpct]%',
+  tree: 1,
+  data: {
+    aCpct: 100
+  }
+},{
   title: '行迹-军势：布洛妮娅在场时，我方全体造成的伤害提高10%',
   check: ({ trees }) => trees[103],
   data: {
     dmg: 10
   }
-},{title: '5.30最后修改：如有问题请输入 #伤害计算反馈'}]
+},{title: '2.23最后修改：如有问题请输入 #伤害计算反馈'}]
