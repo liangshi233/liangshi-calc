5401ed7175804ef22d36ae9e207e7bfb24faefba
liangshi <1142607614@qq.com>
2024-03-10 00:29:18 +0800

    为钟离超全计算适配新版设置

--- damage/meta-gs/钟离/calc_liangshiQ.js
+++ damage/meta-gs/钟离/calc_liangshiQ.js
@@ -1,24 +1,114 @@
 import { Format } from '../../../../../plugins/liangshi-calc/components/index.js'
+import LSconfig from '../../../../../plugins/liangshi-calc/components/LSconfig.js'
+
+let cfg = LSconfig.getConfig('user', 'config')
+let NamePath = cfg.namemodel
+let rankingOnePath = cfg.rankingOnemodel
+let rankingTwoPath = cfg.rankingTwomodel
+let rankingThreePath = cfg.rankingThreemodel
+let gs30ranking = cfg.gs30ranking
+let energy = cfg.energymodel
+let hName = '玉璋护盾'
+let aName = '普通攻击'
+let a2Name = '重击'
+let a3Name = '下落攻击'
+let eName = '地心'
+let e2Name = '地心·磐礴'
+let eNameT = 'E'
+let e2NameT = '长E'
+let qName = '天星'
+let qNameT = 'Q'
+if ( NamePath !== 1 ) {
+ if ( NamePath == 2 ) {
+  aName = '岩雨'
+  eNameT = '地心'
+  e2NameT = '地心·磐礴'
+  qNameT = '天星'
+ } else if ( NamePath == 3 ) {
+  eNameT = '地心'
+  e2NameT = '地心·磐礴'
+  qNameT = '天星'
+ } else if ( NamePath == 4 ) {
+  hName = '元素战技护盾'
+  eName = '元素战技'
+  qName = '元素爆发'
+  eNameT = '元素战技'
+  e2NameT = '长按元素战技'
+  qNameT = '元素爆发'
+ } else if ( NamePath == 5 ) {
+  hName = 'E技能盾'
+  aName = '普攻'
+  a3Name = '下落'
+  eName = 'E技能'
+  qName = 'Q技能'
+  eNameT = 'E技能'
+  e2NameT = '长按E技能'
+  qNameT = 'Q技能'
+ } else if ( NamePath == 6 ) {
+  hName = 'E盾'
+  aName = 'A'
+  a2Name = 'Z'
+  a3Name = '戳'
+  eName = 'E'
+  qName = 'Q'
+  eNameT = 'E'
+  qNameT = 'Q'
+ }
+}
+const miss = ['f', 'y','hph','hps']
+let ranking = 'undefined'
+if (!cfg.gs30ranking) {
+ if ( rankingOnePath == 'm' )  {
+ ranking = 'h'
+ } else if (miss.includes(rankingOnePath)) {
+   if ( rankingTwoPath == 'm' )  {
+    ranking = 'h'
+   }  else if (miss.includes(rankingTwoPath)) {
+     if ( rankingThreePath == 'm' )  {
+      ranking = 'h'
+     }  else if (miss.includes(rankingThreePath)) {
+      logger.mark('[钟离] 排名规则均未命中，已选择默认排名规则')
+      ranking = 'h'
+     }  else {
+       ranking = `${rankingThreePath}`
+     }
+   }  else {
+     ranking = `${rankingTwoPath}`
+   }
+ } else {
+  ranking = `${rankingOnePath}`
+ }
+} else {
+ ranking = `${gs30ranking}`
+}
+if (!cfg.namemodel) {
+energy = 0
+}
+let renew = '11.28-修正岩脊伤害异常问题'
+ renew = '1.29-修正多段类普攻无法多次获取伤害值提升类buff的问题'
+let information = '如有问题请输入 #伤害计算反馈'
 
 export const details = [
 {
-  title: '普通攻击一段伤害',
+  title: `${aName}一段伤害`,
+  dmgKey: 'undefined',
   dmg: ({ talent }, dmg) => dmg(talent.a['一段伤害'], 'a', 'phy')
 },
 {
-  title: '普通攻击二段伤害',
+  title: `${aName}二段伤害`,
   dmg: ({ talent }, dmg) => dmg(talent.a['二段伤害'], 'a', 'phy')
 },
 {
-  title: '普通攻击三段伤害',
+  title: `${aName}三段伤害`,
   dmg: ({ talent }, dmg) => dmg(talent.a['三段伤害'], 'a', 'phy')
 },
 {
-  title: '普通攻击四段伤害',
+  title: `${aName}四段伤害`,
   dmg: ({ talent }, dmg) => dmg(talent.a['四段伤害'], 'a', 'phy')
 },
 {
-  title: '普通攻击五段伤害',
+  title: `${aName}五段伤害`,
+  dmgKey: 'a',
   dmg: ({ talent }, dmg) => {
     let a1 = dmg(talent.a['五段伤害'] / 4 , 'a', 'phy')
     return {
@@ -28,11 +118,12 @@ export const details = [
   }
 },
 {
-  title: '普通攻击六段伤害',
+  title: `${aName}六段伤害`,
   dmg: ({ talent }, dmg) => dmg(talent.a['六段伤害'], 'a', 'phy')
 },
 {
-  title: '重击伤害',
+  title: `${a2Name}伤害`,
+  dmgKey: 'z',
   dmg: ({ talent }, dmg) => dmg(talent.a['重击伤害'], 'a2', 'phy')
 },
 {
@@ -40,15 +131,16 @@ export const details = [
   dmg: ({ talent }, dmg) => dmg(talent.a['下坠期间伤害'], 'a3', 'phy')
 },
 {
-  title: '低空下落伤害',
+  title: `低空${a3Name}伤害`,
   dmg: ({ talent }, dmg) => dmg(talent.a['低空/高空坠地冲击伤害'][0], 'a3', 'phy')
 },
 {
-  title: '高空下落伤害',
+  title: `高空${a3Name}伤害`,
+  dmgKey: 'c',
   dmg: ({ talent }, dmg) => dmg(talent.a['低空/高空坠地冲击伤害'][1], 'a3', 'phy')
 },
 {
-  title: '地心伤害',
+  title: `${eName}伤害`,
   dmg: ({ talent }, dmg) => dmg(talent.e['岩脊伤害/共鸣伤害'][0], 'e')
 },
 {
@@ -60,21 +152,24 @@ export const details = [
   dmg: ({ talent }, dmg) => dmg(talent.e['岩脊伤害/共鸣伤害'][1] * 15 , 'e')
 },
 {
-  title: '地心·磐礴伤害',
+  title: `${e2Name}伤害`,
+  dmgKey: 'e',
   dmg: ({ talent }, dmg) => dmg(talent.e['长按伤害'], 'e')
 },
 {
-  title: '玉璋护盾量',
-  dmgKey: 'hd',
+  title: `${hName}量`,
+  dmgKey: 'h',
   params: { hd: true },
   dmg: ({ attr, calc, talent }, { shield }) => shield(talent.e['护盾基础吸收量'] + calc(attr.hp) * talent.e['护盾附加吸收量'] / 100)
 },
 {
-  title: '天星伤害',
+  title: `${qName}伤害`,
+  dmgKey: 'q',
   dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害'], 'q')
 },
  {
   title: '单人站场18秒',
+  dmgKey: 'dph',
   dmg: ({ talent , cons }, dmg) => {
     let a1 = dmg(talent.a['一段伤害'], 'a', 'phy')
     let a2 = dmg(talent.a['二段伤害'], 'a', 'phy')
@@ -148,6 +243,7 @@ export const details = [
 },
 {
   title: '单人站场DPS',
+  dmgKey: 'dps',
   dmg: ({ talent , calc , attr , weapon , cons }, dmg) => {
     let a1 = dmg(talent.a['一段伤害'], 'a', 'phy')
     let a2 = dmg(talent.a['二段伤害'], 'a', 'phy')
@@ -212,7 +308,7 @@ export const details = [
   }
 },
 {
-  title: '钟鹤云重 钟离普攻前五段',
+  title: `钟鹤云重 ${aName}前五段`,
   params: { teamA: true },
   dmg: ({ talent }, dmg) => {
     let t1 = dmg(talent.a['一段伤害'], 'a')
@@ -227,12 +323,12 @@ export const details = [
   }
 },
 {
-  title: '胡行夜钟 护盾启动',
+  title: `胡行夜钟 ${e2NameT}伤害`,
   params: { teamB: true },
   dmg: ({ talent }, dmg) => dmg(talent.e['长按伤害'], 'e')
 },
 {
-  title: '胡行夜钟 天星伤害',
+  title: `胡行夜钟 ${qNameT}伤害`,
   params: { teamB: true },
   dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害'], 'q')
 },
@@ -242,12 +338,12 @@ export const details = [
   dmg: ({ talent }, dmg) => dmg(talent.e['岩脊伤害/共鸣伤害'][1] , 'e')
 },
 {
-  title: '钟阿荧凝 天星伤害',
+  title: `钟阿荧凝 ${qNameT}伤害`,
   params: { teamC: true },
   dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害'], 'q')
 }]
 
-export const defDmgKey = 'hd'
+export const defDmgKey = `${ranking}`
 export const mainAttr = 'hp,atk,cpct,cdmg'
 
 export const buffs = [
@@ -285,6 +381,7 @@ export const buffs = [
 {
   check: ({ cons , params }) => cons >= 2 && params.teamA === true,
   title: '申鹤2命：[定蒙] 神女遣灵真诀领域中的当前场上角色，冰元素伤害的暴击伤害提高[aCdmg]%',
+  sort: 1,
   data: {
     aCdmg: 15
   }
@@ -301,7 +398,7 @@ export const buffs = [
   title: '申鹤技能：[仰灵威召将役咒] 普通攻击对敌人造成冰元素伤害时,造成的伤害提高[aPlus]%',
   sort: 9,
   data: {
-    aPlus: 4009.0
+    aPlus: 6080.0
   }
 },
 {
@@ -309,12 +406,13 @@ export const buffs = [
   title: '申鹤技能：[仰灵威召将役咒] 普通攻击对敌人造成冰元素伤害时,造成的伤害提高[aPlus]%',
   sort: 9,
   data: {
-    aPlus: 2767.2
+    aPlus: 4375.2
   }
 },
 {
   check: ({ params }) => params.teamA === true,
   title: '申鹤天赋：[缚灵通真法印] 申鹤施放仰灵威召将役咒后将使附近的队伍中所有角色根据技能释放形式对应伤害提高[dmg]%',
+  sort: 1,
   data: {
     dmg: 15
   }
@@ -322,6 +420,7 @@ export const buffs = [
 {
   check: ({ params }) => params.teamA === true,
   title: '申鹤天赋：[大洞弥罗尊法] 处于神女遣灵真诀的领域中的当前场上角色，冰元素伤害加成提高[dmg]%',
+  sort: 1,
   data: {
     dmg: 15
   }
@@ -368,8 +467,8 @@ export const buffs = [
   }
 },
 {
-  check: ({ params , artis }) => params.teamA === true && artis.千岩牢固 !== 4 ,
-  title: '重云圣遗物：[千岩牢固4] 元素战技命中敌人使队伍中附近的所有角色攻击力提升[atkPct]%护盾强效提升[shield]% { 该圣遗物效果不可叠加 }',
+  check: ({ params }) => params.teamA === true,
+  title: '重云圣遗物：[千岩牢固4] 元素战技命中敌人使队伍中附近的所有角色攻击力提升[atkPct]%护盾强效提升[shield]%',
   sort: 1,
   data: {
     atkPct: 20,
@@ -400,6 +499,22 @@ export const buffs = [
   }
 },
 {
+  check: ({ params }) => params.teamA === true,
+  title: '元素共鸣：[坚定之岩] 护盾强效提升[shield]%，造成的伤害提升[dmg]%，降低敌人[kx]%岩元素抗性',
+  data: {
+    shield: 25,
+    dmg: 15,
+    kx: 20
+  }
+},
+{
+  check: ({ params }) => params.teamA === true,
+  title: '元素共鸣：[粉碎之冰] 攻击处于冰元素附着或冻结下的敌人时,暴击率提高[cpct]%',
+  data: {
+    cpct: 15
+  }
+},
+{
   check: ({ params }) => params.teamB === true,
   title: '胡桃天赋：[蝶隐之时] 蝶引来生施加的彼岸蝶舞状态结束后，队伍中所有角色的暴击率提高[qCpct]%',
   data: {
@@ -415,11 +530,10 @@ export const buffs = [
   }
 },
 {
-  check: ({ params , cons }) => cons >= 4 && params.teamB === true,
-  title: '夜兰4命：[诓惑者，接树移花] 依照「络命丝」标记敌人的数量,使队伍中所有角色的生命值上限提升[hpPct]%',
-  sort: 1,
+  check: ({ params }) => params.teamB === true,
+  title: '元素共鸣：[愈疗之水] 生命值上限提升[hpPct]%',
   data: {
-    hpPct: 40
+  	hpPct: 25
   }
 },
 {
@@ -467,29 +581,6 @@ export const buffs = [
   }
 },
 {
-  check: ({ params }) => params.teamA === true,
-  title: '元素共鸣：[坚定之岩] 护盾强效提升[shield]%，造成的伤害提升[dmg]%，降低敌人[kx]%岩元素抗性',
-  data: {
-    shield: 25,
-    dmg: 15,
-    kx: 20
-  }
-},
-{
-  check: ({ params }) => params.teamA === true,
-  title: '元素共鸣：[粉碎之冰] 攻击处于冰元素附着或冻结下的敌人时,暴击率提高[cpct]%',
-  data: {
-    cpct: 15
-  }
-},
-{
-  check: ({ params }) => params.teamB === true,
-  title: '元素共鸣：[愈疗之水] 生命值上限提升[hpPct]%',
-  data: {
-  	hpPct: 25
-  }
-},
-{
   check: ({ params }) => params.teamC === true,
   title: '元素共鸣：[坚定之岩] 护盾强效提升[shield]%，造成的伤害提升[dmg]%，降低敌人[kx]%岩元素抗性',
   data: {
@@ -498,9 +589,4 @@ export const buffs = [
     kx: 20
   }
 },
-{title: '1.29最后修改：[10.19重置] 修正多段类普攻无法多次获取伤害值提升类buff的问题'}
-]
-/*
-这里放的是历史更新日志
-{title: '11.28最后修改：[10.19重置] 修正岩脊伤害异常问题'}
-*/
+{title: `2.29最后修改：[10.19重置] 显示模式:${NamePath} 排行设置:${rankingOnePath},${rankingTwoPath},${rankingThreePath} 专属排行设置:${gs30ranking} 魔物产球设置:${energy} 更新日志:${renew} 其他信息:${information}`}]
