09035bf2d90a76a3f4c16f6bd6759425c230d585
liangshi <1142607614@qq.com>
2024-04-17 00:09:02 +0800

    开拓者·毁灭极限面板

--- apps/beta.js
+++ apps/beta.js
@@ -7,7 +7,7 @@ export class example extends plugin {
 		event: 'message',
 		priority: -1,
 		rule: [
-// 不需要此功能请将下方两个 // 删除
+// 每个角色已分开，不需要此功能或不需要的角色请将对应的两个 // 删除
 //  /*
 		  {
 			reg: '^#?(知更鸟(极限面板|面板100000000)|极限知更鸟)$',
@@ -17,10 +17,17 @@ export class example extends plugin {
 /*
 		  ,
 		  {
-			reg: '^#?(知更鸟(极限面板|面板100000000)|极限知更鸟)$',
+			reg: '^#?(知更鸟(极限伤害|伤害100000000))$',
 			fnc: 'sh'
 		  }
 */
+//  /*
+		  ,
+		  {
+			reg: '^#?((开拓者·毁灭|开拓者•毁灭|开拓者毁灭|毁灭开拓者|物理开拓者|开拓者物理|毁灭主|物理主|物主)(极限面板|面板100000000))$',
+			fnc: 'wlktz'
+		  }
+//  */
 		]
 	  })
 	}
@@ -32,6 +39,11 @@ export class example extends plugin {
 
  async sh(e) {
   e.reply(segment.image(`file:///${_path}/plugins/liangshi-calc/resources/2.bmp`))
+  return
+ }
+
+ async wlktz(e) {
+  e.reply(segment.image(`file:///${_path}/plugins/liangshi-calc/resources/3.bmp`))
   return
  }
 
