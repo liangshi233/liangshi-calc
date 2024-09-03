6f938077588977a0799380a31d6cf99744a26bba
liangshi <1142607614@qq.com>
2024-09-03 19:17:35 +0800

    .

---
+++ damage/meta-gs/玛拉妮/artis_liangshi.js
@@ -0,0 +1,21 @@
+export default function ({ attr, artis, weapon, rule, def }) {
+  if (weapon.name === '试作金珀' && attr.heal > 1) {
+    return rule('生存-治疗', { atk: 30, cpct: 60, cdmg: 60, dmg: 60, recharge: 100, heal: 100 })
+  }
+  if ((weapon.name === '昭心' || weapon.name === '天空之卷') && attr.phy > 15) {
+    return rule('驻场-物理', { atk: 85, cpct: 100, cdmg: 100, phy: 100, recharge: 15 })
+  }
+  if (artis.is('乐园4') || attr.mastery >= 540) {
+    return rule('驻场-绽放', { hp: 55, cpct: 60, cdmg: 60, mastery: 100, dmg: 60, recharge: 80 })
+  }
+  if (artis.is('冰套4')) {
+    return rule('驻场-冻结', { hp: 85, cpct: 100, cdmg: 100, dmg: 100, recharge: 45 })
+  }
+  if (attr.mastery < 80) {
+    return rule('驻场-直伤', { hp: 85, cpct: 100, cdmg: 100, dmg: 100, recharge: 45 })
+  }
+  if (attr.mastery >= 80 && attr.mastery < 540) {
+    return rule('驻场-蒸发', { hp: 75, cpct: 100, cdmg: 100, mastery: 50, dmg: 100, recharge: 45 })
+  }
+  return def({ hp: 75, cpct: 100, cdmg: 100, mastery: 50, dmg: 100, recharge: 45 })
+}
