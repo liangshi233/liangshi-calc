a5d5c217413bad16526626f319d7774da95b3a1c
unknown <1446649646@qq.com>
2024-10-07 18:49:03 +0800

    公子蒸发触发条件降低

--- damage/liangshi-gs/达达利亚/artis_liangshi.js
+++ damage/liangshi-gs/达达利亚/artis_liangshi.js
@@ -2,13 +2,13 @@ export default function ({ attr, artis, rule, def }) {
   if (attr.phy > 52) {
     return rule('驻场-物理', { atk: 85, cpct: 100, cdmg: 100, phy: 100, recharge: 15 })
   }
-  if (attr.mastery < 120) {
+  if (attr.mastery < 80) {
     return rule('驻场-直伤', { atk: 85, cpct: 100, cdmg: 100, dmg: 100, recharge: 45 })
   }
   if (artis.is('冰套4')) {
     return rule('驻场-冻结', { atk: 85, cpct: 100, cdmg: 100, dmg: 100, recharge: 45 })
   }
-  if (attr.mastery >= 120 && attr.mastery < 540) {
+  if (attr.mastery >= 80 && attr.mastery < 540) {
     return rule('驻场-蒸发', { atk: 75, cpct: 100, cdmg: 100, mastery: 50, dmg: 100, recharge: 45 })
   }
   if (artis.is('乐园4') || attr.mastery >= 540) {
