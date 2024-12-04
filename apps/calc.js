8fe691e9c28a4dd7672edcb510f0e15eb7d084a7
liangshi <1142607614@qq.com>
2024-12-04 13:48:58 +0800

    修复无法使用链接安装其他伤害计算

--- apps/calc.js
+++ apps/calc.js
@@ -16,7 +16,7 @@ export class calc extends plugin {
          priority: 5000,
          rule: [
           {
-            reg: '^#*(强制)?更新(.*)(崩坏三|崩坏3|崩三|崩3|bh3|BH3|原神|ys|YS|gs|GS|星铁|sr|SR|崩坏星穹铁道|崩坏：星穹铁道|铁道|绝区零|zzz|ZZZ|绝|鸣潮|mc|MC|明朝|潮|幻塔|HT|ht|幻塔PSN|幻塔psn|HTPSN|htpsn|战双|ZS|zs|战双帕弥什|帕弥什|PNS|pns|尘白|CBJQ|cbjq|尘白禁区)(计算|伤害计算)$',
+            reg: '^#*(强制)?更新(.*)(崩坏三|崩坏3|崩三|崩3|原神|星铁|崩坏星穹铁道|崩坏：星穹铁道|铁道|绝区零|绝|鸣潮|明朝|潮|幻塔|幻塔PSN|幻塔psn|战双|战双帕弥什|帕弥什|尘白|尘白禁区)(.*?)(计算|伤害计算)$',
             fnc: 'calc'
           }
         ]
@@ -33,41 +33,42 @@ export class calc extends plugin {
     let calcdata = ''
     if (/原神|ys|YS|gs|GS/.test(this.e.msg)) {
       calcdata = 'gs'
-    } else if (/星铁|sr|SR|崩坏星穹铁道|崩坏：星穹铁道|铁道/.test(this.e.msg)) {
+    } else if (/星铁|崩坏星穹铁道|崩坏：星穹铁道|铁道/.test(this.e.msg)) {
       calcdata = 'sr'
-    } else if (/鸣潮|mc|MC|明朝|潮/.test(this.e.msg)) {
+    } else if (/鸣潮|明朝|潮/.test(this.e.msg)) {
       calcdata = 'mc'
-    } else if (/绝区零|zzz|ZZZ|绝/.test(this.e.msg)) {
+    } else if (/绝区零|绝/.test(this.e.msg)) {
       calcdata = 'zzz'
-    } else if (/崩坏三|崩坏3|崩三|崩3|bh3|BH3/.test(this.e.msg)) {
+    } else if (/崩坏三|崩坏3|崩三|崩3/.test(this.e.msg)) {
       calcdata = 'bh3'
-    } else if (/幻塔PSN|幻塔psn|HTPSN|htpsn/.test(this.e.msg)) {
+    } else if (/幻塔PSN|幻塔psn/.test(this.e.msg)) {
       calcdata = 'htpsn'
-    } else if (/幻塔|HT|ht/.test(this.e.msg)) {
+    } else if (/幻塔/.test(this.e.msg)) {
       calcdata = 'ht'
-    } else if (/战双|ZS|zs|战双帕弥什|帕弥什|PNS|pns/.test(this.e.msg)) {
+    } else if (/战双|战双帕弥什|帕弥什/.test(this.e.msg)) {
       calcdata = 'pns'
-    } else if (/尘白|CBJQ|cbjq|尘白禁区/.test(this.e.msg)) {
+    } else if (/尘白|尘白禁区/.test(this.e.msg)) {
       calcdata = 'cbjq'
     } else {
       calcdata = 'gs' //仅添加已支持的游戏，如有新的游戏可在issue中提出
     } //部分游戏可能需要安装相应插件才可使用且部分游戏可能拥有多个插件，请安装前注意插件名称
     let namedata = ''
     let gitdata = ''
-    if (/liangshi|梁氏|LS|Liangshi|ls/.test(this.e.msg)) {
+    if (/更新(liangshi|梁氏|LS|Liangshi|ls)/.test(this.e.msg)) {
       gitdata = 'https://gitee.com/liangshi233'
       namedata = 'liangshi'
-    } else if (/ikun|Ikun/.test(this.e.msg)) {
+    } else if (/更新(ikun|Ikun)/.test(this.e.msg)) {
       gitdata = 'https://gitee.com/jntmtt'
       namedata = 'ikun'
-    } else if (/当前|已启用|使用中|默认|自定义/.test(this.e.msg)) {
+    } else if (/更新(当前|已启用|使用中|默认|自定义)/.test(this.e.msg)) {
       namedata = cfgL.calcmodel
     } else {
-     let webdata = e.msg.match(/^#*(强制)?更新(.*)(崩坏三|崩坏3|崩三|崩3|bh3|BH3|原神|ys|YS|gs|GS|星铁|sr|SR|崩坏星穹铁道|崩坏：星穹铁道|铁道|绝区零|zzz|ZZZ|绝|鸣潮|mc|MC|明朝|潮|幻塔|HT|ht|幻塔PSN|幻塔psn|HTPSN|htpsn|战双|ZS|zs|战双帕弥什|帕弥什|PNS|pns|尘白|CBJQ|cbjq|尘白禁区)(计算|伤害计算)$/)
+     let webdata = e.msg.match(/^#*(强制)?更新(.*)(崩坏三|崩坏3|崩三|崩3|原神|星铁|崩坏星穹铁道|崩坏：星穹铁道|铁道|绝区零|绝|鸣潮|明朝|潮|幻塔|幻塔PSN|幻塔psn|战双|战双帕弥什|帕弥什|尘白|尘白禁区)(.*?)(计算|伤害计算)$/)
      if (fs.existsSync(`${_path}/plugins/liangshi-calc/damage/${webdata[2]}-${calcdata}/`)) {
       namedata = `${webdata[2]}`
      } else {
       gitdata = `${webdata[2]}`
+      namedata = `${webdata[4]}`
      } //部分游戏插件和部分作者的计算为私库状态，需要根据相应插件说明在此添加对应内容
     }
     let command = ''
