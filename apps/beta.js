6070eeb461ae00d09c97a153412e9d10ab0cdc83
liangshi <1142607614@qq.com>
2025-01-01 22:15:15 +0800

    5.4

---
+++ apps/beta.js
@@ -0,0 +1,27 @@
+const _path = process.cwd()
+
+export class beta extends plugin {
+  constructor () {
+    super({
+      name: '测试角色极限面板',
+      dsc: '测试角色极限面板',
+      event: 'message',
+      priority: -1,
+      rule: [
+        // 每个角色已分开，不需要此功能或不需要的角色请将对应的注释
+        {
+          reg: '^#?((极限|极限面板)?(梦见月瑞希|月瑞希)(极限面板|面板100000000))$',
+          fnc: 'mjyrx'
+        }
+      ]
+    })
+  }
+
+  async mjyrx () {
+    this.e.reply(segment.image(`file:///${_path}/plugins/liangshi-calc/resources/1.jpeg`))
+    return true
+  }
+
+
+
+}
