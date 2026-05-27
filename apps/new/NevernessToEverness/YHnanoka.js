9b368fc94aea57e3a657ff46f3cb77b938ff23b5
liangshi <1142607614@qq.com>
2026-05-27 15:26:51 +0800

    一键更新新增异环武器适配

--- apps/new/NevernessToEverness/YHnanoka.js
+++ apps/new/NevernessToEverness/YHnanoka.js
@@ -360,7 +360,178 @@ export async function CharacterNew (e, mode, JsonOk) {
   }
 }
 
-export async function WeaponNew (e, mode, JsonOk) { if(!mode) e.reply('[liangshi-calc]暂不支持使用此API更新(ಥ_ಥ)\n请在设置中切换API后再试'); return false }
+export async function WeaponNew (e, mode, JsonOk) {
+  if (!e.isMaster) { e.reply('你不可以更新哦~(*/ω＼*)'); return false }
+  let cfg = LSconfig.getConfig('user', 'config')
+  let TextData = e.msg.match(/^#*(梁氏|liangshi)?(强制|强行|覆盖)?更新(异环|YH|yh|NTE|nte)(.*?)(武器|光锥|弧盘)(数据|资源|资源数据)?(.*?)$/)
+  let WeaponId = TextData[4], verLeve
+  try {
+    if (/^\d{5}$/.test(WeaponId) || /强制|强行|覆盖/.test(e.msg)) {
+      console.log(`[liangshi-calc]开始更新ID:${WeaponId}的弧盘数据`)
+      if (!mode) e.reply(`[liangshi-calc]开始更新ID:${WeaponId}的弧盘数据`)
+    } else {
+      console.error(`[liangshi-calc]未知的弧盘ID:${WeaponId}`)
+      if (!mode) e.reply('[liangshi-calc]弧盘ID错误，请检查弧盘ID格式')
+      return false
+    }
+    let response, ProxyUrl, WeaponData, url, data, verUrl, itemdata
+    if (cfg.ProxyUrl) { ProxyUrl = cfg.ProxyUrl } else { ProxyUrl = "" }
+    try {
+      verUrl = await fetch(`${ProxyUrl}https://static.nanoka.cc/manifest.json`)
+      verUrl = await verUrl.json()
+      verLeve = verUrl.nte.latest
+      url = `${ProxyUrl}https://static.nanoka.cc/nte/${verLeve}/zh/weapon/${WeaponId}.json`
+      response = await fetch(url)
+      if (!response.ok) { console.error(`[liangshi-calc]访问云端时发生错误:${response.status}`); throw new Error() }
+      data = await response.json()
+      console.log(`[liangshi-calc]弧盘：${data.name || "无名"} 云端数据读取成功`)
+    } catch (err) {
+      console.error("[liangshi-calc]云端拉取数据时发生错误\n", err)
+      if (response.status === 404) {
+        if (!mode) e.reply('[liangshi-calc]云端暂无该弧盘数据，可等待一段时间后再更新')
+      } else if (response.status === 429) {
+        if (!mode) e.reply('[liangshi-calc]你更新的速度太快了，请稍等一下再试吧(*/ω＼*)')
+      } else if (response.status >= 500) {
+        if (!mode) e.reply('[liangshi-calc]云端服务器可能正在维护，请稍等一下再试吧(*/ω＼*)')
+      } else if (cfg.ProxyUrl) {
+        if (!mode) e.reply('[liangshi-calc]请求异常，可能是网络超时，建议检查配置的代理后再试(*/ω＼*)')
+      } else {
+        if (!mode) e.reply('[liangshi-calc]请求异常，可能是网络超时，建议使用代理后再试(*/ω＼*)')
+      }
+      return false
+    }
+    if (!JsonOk || !fs.existsSync("./plugins/liangshi-calc/resources/ItemJson.json")) {
+      response = await fetch(`${ProxyUrl}https://static.nanoka.cc/nte/${verLeve}/zh/item.json`)
+      if (!response.ok) {
+        console.error(`[liangshi-calc]访问云端时发生错误:${response.status}`)
+        if (response.status === 404) {
+          if (!mode) e.reply('[liangshi-calc]云端暂无该物品数据，可等待一段时间后再更新')
+        } else if (response.status === 429) {
+          if (!mode) e.reply('[liangshi-calc]你查询的速度太快了，请稍等一下再试吧(*/ω＼*)')
+        } else if (response.status >= 500) {
+          if (!mode) e.reply('[liangshi-calc]云端服务器可能正在维护，请稍等一下再试吧(*/ω＼*)')
+        } else if (cfg.ProxyUrl) {
+          if (!mode) e.reply('[liangshi-calc]请求异常，可能是网络超时，建议检查配置的代理后再试(*/ω＼*)')
+        } else {
+          if (!mode) e.reply('[liangshi-calc]请求异常，可能是网络超时，建议使用代理后再试(*/ω＼*)')
+        }
+        return false
+      }
+      itemdata = await response.json()
+      console.log(`[liangshi-calc]云端数据读取成功`)
+    } else {
+      response = fs.readFileSync("./plugins/liangshi-calc/resources/ItemJson.json", 'utf8')
+      itemdata = JSON.parse(response)
+      console.log(`[liangshi-calc]本地数据读取成功`)
+    }
+    let type = data.type_name, WeaponName = data.name
+    if (!fs.existsSync(`./plugins/miao-plugin/resources/meta-yh/weapon/${type}/${WeaponName}`) || /强制|强行|覆盖/.test(e.msg)) {
+      if (!mode) e.reply(`[liangshi-calc]开始更新弧盘: ${WeaponName}`)
+      fs.mkdirSync(`./plugins/miao-plugin/resources/meta-yh/weapon/${type}/${WeaponName}`, { recursive: true })
+      console.log(`[liangshi-calc]弧盘:${WeaponName} 本地文件夹创建成功`)
+    } else { if (!mode) e.reply(`[liangshi-calc]弧盘: ${WeaponName} 已经存在，如需更新数据请使用覆盖更新。`); return false }
+
+    WeaponData = {
+      "id": WeaponId,
+      "name": WeaponName,
+      "star": data.rarity,
+      "desc": data.description,
+      "type": type,
+      "attr": {
+        "atk": {
+          "1": data.stats[0].values[0],
+          "20": data.stats[0].values[19],
+          "30": data.stats[0].values[29],
+          "40": data.stats[0].values[39],
+          "50": data.stats[0].values[49],
+          "60": data.stats[0].values[59],
+          "70": data.stats[0].values[69],
+          "80": data.stats[0].values[79]
+        },
+        "bonusKey": data.stats[1].name,
+        "bonusData": {
+          "1": data.stats[1].values[0],
+          "20": data.stats[1].values[19],
+          "30": data.stats[1].values[29],
+          "40": data.stats[1].values[39],
+          "50": data.stats[1].values[49],
+          "60": data.stats[1].values[59],
+          "70": data.stats[1].values[69],
+          "80": data.stats[1].values[79]
+        }
+      },
+      "materials": {
+        "weapon": Object.values(itemdata).find(v => v.id === data.materials[5].items_id[0])?.name,
+        "monster": Object.values(itemdata).find(v => v.id === data.materials[5].items_id[1])?.name
+      },
+      "skill": {
+        "name": data.effect?.name,
+        "desc": data.effect.description.replace(/<lv>\{(\d+)\}<\/>/g, '$[$1]').replace(/\r\n/g, ''),
+        "tables": data.effect?.values.map(item => item.values)
+      }
+    }
+    console.log('[liangshi-calc]数据处理完成')
+    let path = `./plugins/miao-plugin/resources/meta-yh/weapon/${type}/${WeaponName}/data.json`
+    if (!fs.existsSync(path)) {
+      fs.writeFileSync(path, JSON.stringify(WeaponData, null, 2), 'utf8')
+      console.log(`[liangshi-calc]弧盘：${WeaponName} 数据已写入`)
+      if (!mode) e.reply(`[liangshi-calc]弧盘：${WeaponName}\n数据已写入`)
+    } else if (/强制|强行|覆盖/.test(e.msg)) {
+      if (!mode) e.reply('[liangshi-calc]弧盘数据已存在，当前为强制模式，尝试覆盖写入。')
+      fs.writeFileSync(path, JSON.stringify(WeaponData, null, 2), 'utf8')
+      console.log(`[liangshi-calc]弧盘：${WeaponName} 数据已写入`)
+      if (!mode) e.reply(`[liangshi-calc]弧盘：${WeaponName}\n数据已写入`)
+    } else {
+      if (!mode) e.reply(`[liangshi-calc]弧盘数据已存在，运行终止。\n如果需要刷新弧盘数据至最新预览版本请使用覆盖更新\n例：#覆盖更新${WeaponId}弧盘数据`)
+      console.error(`[liangshi-calc]弧盘：${WeaponName}\n数据已存在`)
+      return false
+    }
+    console.log(`[liangshi-calc]开始下载弧盘图片资源`)
+    let imgs = `./plugins/miao-plugin/resources/meta-yh/weapon/${type}/${WeaponName}`
+    await getImg(ProxyUrl + `https://static.nanoka.cc/assets/nte${data.icon}.webp`, `${imgs}/icon-s.webp`, "icon-s")
+    await getImg(ProxyUrl + `https://static.nanoka.cc/assets/nte${data.card_icon}.webp`, `${imgs}/splash.webp`, "splash")
+    await getImg(ProxyUrl + `https://static.nanoka.cc/assets/nte${data.original_icon}.webp`, `${imgs}/icon.webp`, "icon")
+    if (!mode) e.reply(`[liangshi-calc]弧盘图片资源下载完成`)
+    console.log(`[liangshi-calc]图片资源下载完成`)
+    if (cfg.AutoUpdateData || /强制|强行|覆盖/.test(e.msg)) {
+      let filePath = `./plugins/miao-plugin/resources/meta-yh/weapon/data.json`
+      if (!fs.existsSync(filePath)) { fs.writeFileSync(filePath, '{}'); console.log(`[liangshi-calc]未找到data.json文件，已自动创建`) }
+      fs.readFile(filePath, 'utf8', (err, TextData) => {
+        if (err) {
+          console.error('[liangshi-calc]读取弧盘配置data.json失败:', err)
+          if (!mode) e.reply(`[liangshi-calc]弧盘：${WeaponName} 数据更新完成\n尝试自动写入WeaponData时失败\n请手动添加后重启使用`)
+          return false
+        }
+        try {
+          let jsonData = JSON.parse(TextData)
+          jsonData[WeaponId] = { "id": WeaponId, "name": WeaponName, "type": type, "star": data.rarity }
+          console.log(`[liangshi-calc]弧盘：${WeaponName} 配置data.json成功`)
+          let updatedData = JSON.stringify(jsonData, null, 2)
+          fs.writeFile(filePath, updatedData, 'utf8', (err) => {
+            if (err) {
+              console.error('[liangshi-calc]弧盘data.json写入失败:\n', err)
+              if (!mode) e.reply(`[liangshi-calc]弧盘：${WeaponName} 数据更新完成\n尝试自动写入WeaponData时失败\n请手动添加后重启使用`)
+              return false
+            } else { console.log('[liangshi-calc]弧盘data.json已更新') }
+          })
+        } catch (err) { console.error('[liangshi-calc]自动配置data.json失败:\n', err) }
+      })
+      if (!mode) e.reply(`[liangshi-calc]弧盘：${WeaponName} 数据更新完成\n重启后即可使用相关内容`)
+    } else { if (!mode) e.reply(`[liangshi-calc]弧盘：${WeaponName} 数据更新完成\n当前未启用自动写入WeaponData\n手动配置后重启才可使用\n自动写入WeaponData可在config.yaml启用或使用强制更新临时启用一次`)}
+    return false
+  } catch (err) {
+    if (!mode) { e.reply(`[liangshi-calc]更新错误,建议检查网络状态,如网络正常可复制下方信息前往762197317反馈\n\n${err}`) } else {
+      console.error(`[liangshi-calc]更新遇到了一些错误,已跳过此内容更新\n建议使用 #强制更新${TextData[3]}${TextData[4]}弧盘数据 进行手动更新\n${err}`)
+      let lj = "./plugins/liangshi-calc/resources/log.json"
+      let oldLog = fs.existsSync(lj) ? fs.readFileSync(lj, 'utf8') : '{}'
+      let y = JSON.parse(oldLog)
+      y[new Date()] = { name: TextData[4], err, text: "弧盘更新错误" }
+      let bbxzData = JSON.stringify(y, null, 2)
+      fs.writeFile(lj, bbxzData, 'utf8', (err) => { if (err) { console.error('[liangshi-calc]错误内容记录失败:\n', err); return false } else { console.log('[liangshi-calc]错误内容已记录') }})
+    }
+    return true
+  }
+}
 
 export async function ArtifactNew (e, mode, JsonOk) { if(!mode) e.reply('[liangshi-calc]暂不支持使用此API更新(ಥ_ಥ)\n请在设置中切换API后再试'); return false }
 
