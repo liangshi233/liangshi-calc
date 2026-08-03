82469d1020d5c1e25c36dfa028b9d290f94c168e
liangshi <1142607614@qq.com>
2026-08-03 16:40:40 +0800

    补充异环基础数据

--- damage/liangshi-yh/data/weapon.js
+++ damage/liangshi-yh/data/weapon.js
@@ -1,4 +1,177 @@
+export const source = {
+  "地图探索": [
+    "永恒圆舞曲",
+    "引爆全场",
+    "良药苦口",
+    "漆黑青春妄想",
+    "无畏之绵",
+    "不屈之绵",
+    "现实避难所",
+    "扭曲之城的呼唤",
+    "时间大盗",
+    "当心头顶",
+    "凶猛之绵",
+    "极速之绵",
+    "面具下的泪",
+    "开始净空",
+    "「我们。」",
+    "灵敏之绵",
+    "鲸之歌",
+    "「电音」狂欢",
+    "光波眩晕，迷走狂欢",
+    "危险游戏",
+    "闪耀的每一天",
+    "茶花会",
+    "终有时",
+    "灵感大逃杀",
+    "好狗狗走四方",
+    "「银河暂留」",
+    "成功的第一步",
+    "欧拉欧拉！",
+    "愚者种春",
+    "挂你在心口难开",
+    "该死的邂逅",
+    "笑口常开",
+    "「倾世之雨」",
+    "被遗忘者",
+    "焰魂狂飙",
+    "拔刀",
+    "勿忘伞",
+  ],
+  "商店购买": [
+    "千金难买你开心"
+  ],
+  "任务获得": [
+    "海特洛的安宁",
+    "休息日",
+  ],
+  "活动获得": [
+    "思考喵"
+  ]
+}
+
 export const exclusive = {
-  "浔": '',
-  "娜娜莉": ''
+  "伊洛伊": '错误的门',
+  "真红": '穿过胭红蜃景',
+  "卡厄斯": '众人追寻之物',
+  "安魂曲": '「最后一朵玫瑰」',
+  "浔": '行进于时间之外',
+  "娜娜莉": '预备备',
+  "小吱": '思考喵',
+  "零": '倾世之雨'
+}
+
+export const matNum = {
+  "3": {
+    "avatar_material": '252000',
+    "exp": '69',
+    "normal": ["8", "12", "12"],
+    "talent": ["8", "12", "12"]
+  },
+  "4": {
+    "avatar_material": '336000',
+    "exp": '115',
+    "normal": ["11", "15", "15"],
+    "talent": ["11", "15", "15"]
+  },
+  "5": {
+    "avatar_material": '420000',
+    "exp": '135',
+    "normal": ["14", "18", "18"],
+    "talent": ["14", "18", "18"]
+  }
+}
+
+export const attrKey = {
+  "5":{
+    "25": {
+      "front": "<span style=\"color: #62FE49; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #FF4747; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "28": {
+      "front": "<span style=\"color: #A5FF9C; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #FF9B9B; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "31": {
+      "front": "<span style=\"color: #BDFFBD; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #FFBFBD; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "33": {
+      "front": "<span style=\"color: #fff; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #fff; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "37": {
+      "front": "<span style=\"color: #FFBFBD; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #BDFFBD; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "40": {
+      "front": "<span style=\"color: #FF9B9B; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #A5FF9C; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "43": {
+      "front": "<span style=\"color: #FF4747; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #62FE49; font-size: 18px;\">",
+      "after": "</span>"
+    }
+  },
+  "4":{
+    "27": {
+      "front": "<span style=\"color: #62FE49; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #FF4747; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "29": {
+      "front": "<span style=\"color: #A5FF9C; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #FF9B9B; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "31": {
+      "front": "<span style=\"color: #BDFFBD; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #FFBFBD; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "33": {
+      "front": "<span style=\"color: #fff; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #fff; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "35": {
+      "front": "<span style=\"color: #FFBFBD; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #BDFFBD; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "37": {
+      "front": "<span style=\"color: #FF9B9B; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #A5FF9C; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "39": {
+      "front": "<span style=\"color: #FF4747; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #62FE49; font-size: 18px;\">",
+      "after": "</span>"
+    }
+  },
+  "3":{
+    "24": {
+      "front": "<span style=\"color: #BDFFBD; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #FFBFBD; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "25": {
+      "front": "<span style=\"color: #fff; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #fff; font-size: 18px;\">",
+      "after": "</span>"
+    },
+    "26": {
+      "front": "<span style=\"color: #FFBFBD; font-size: 18px;\">",
+      "opFront": "<span style=\"color: #BDFFBD; font-size: 18px;\">",
+      "after": "</span>"
+    }
+  }
 }
